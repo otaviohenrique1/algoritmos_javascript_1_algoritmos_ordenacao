8fce973ad47556863c7a3642780d7a2c2b1451f8
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-01-04 15:44:43 -0300

    Commit 1

--- Aula1/index.js
+++ Aula1/index.js
@@ -1,14 +1,14 @@
-const precosLivros = [25, 15, 30, 50, 45, 20];
+const livros = require('./listaLivros');
 
-// let atual = 0;
 let maisBarato = 0;
 
-for (let atual = 0; atual < precosLivros.length; atual++) {
-  if (precosLivros[atual] < precosLivros[maisBarato]) {
+for (let atual = 0; atual < livros.length; atual++) {
+  if (livros[atual].preco < livros[maisBarato].preco) {
     maisBarato = atual;
   }
 }
 
-console.log(`Mais barato => R$ ${precosLivros[maisBarato]}`);
+console.log(`Mais barato => R$ ${livros[maisBarato]}`);
 
+// const precosLivros = [25, 15, 30, 50, 45, 20];
 // console.log(precosLivros.sort((a, b) => a > b));
