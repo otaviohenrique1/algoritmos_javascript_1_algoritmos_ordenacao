ae82a9a7b090bad1bf5ed6369989a96e74f2f4dd
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-01-05 16:52:11 -0300

    Commit 6

--- Aula3/selectionSort.js
+++ Aula3/selectionSort.js
@@ -1,6 +1,6 @@
 const livros = require('./listaLivros');
-const livros2 = require('./listaLivros');
 const menorValor = require('./menorValor');
+// const livros2 = require('./listaLivros');
 
 // Algoritmo Selection Sort
 for (let atual = 0; atual < livros.length - 1; atual++) {
@@ -15,14 +15,14 @@ for (let atual = 0; atual < livros.length - 1; atual++) {
 
 console.log(livros);
 
-livros2.forEach((livro2, indice) => {
-  let menor = menorValor(livros2, indice);
+// livros2.forEach((livro2, indice) => {
+//   let menor = menorValor(livros2, indice);
 
-  let livroAtual = livros2[indice];
-  let livroMenorPreco = livros2[menor];
+//   let livroAtual = livros2[indice];
+//   let livroMenorPreco = livros2[menor];
 
-  livros2[indice] = livroMenorPreco;
-  livros2[menor] = livroAtual;
-});
+//   livros2[indice] = livroMenorPreco;
+//   livros2[menor] = livroAtual;
+// });
 
-console.log(livros2);
+// console.log(livros2);
